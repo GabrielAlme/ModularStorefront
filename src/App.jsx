ef8a7e9c98c04ad5a79450a3d918257f696d3d1d
Gabriel Alme <gabealmeemail@gmail.com>
2026-09-24 12:08:04 -0500

    fixed syntax for AddPanel dropdown so it loads the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import Workspace from "./components/Workspace"
+import AddPanel from "./components/AddPanel"
 
 const App = () => {
   const [open, setOpen] = useState(["placeholder", "placeholder2", "placeholder3", "placeholder4"]);
   
-  return <Workspace open={open} />
+  return (
+    <>
+      <AddPanel/>
+      <Workspace open={open}/>
+    </>
+  )
 }
 
 export default App
--- src/components/AddPanel.jsx
+++ src/components/AddPanel.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react";
-
-const [isOpen, setIsOpen] = useState(false);
-const [select, setSelect] = useState(null);
-const ref = useRef(null);
-
-useEffect(() => {
-    const handleClick = (e) => {
-        if (ref.current && !ref.current.contains(e.target)) setIsOpen(false);
-    };
-    document.addEventListener("mousedown", handleClick);
-    return () => document.removeEventListener("mousedown", handleClick);
-}, []);
-
-const handleSelect = (item) => {
-    setSelect(item);
-};
+import { useState, useRef, useEffect } from "react";
 
 const AddPanel = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [select, setSelect] = useState(null);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const handleClick = (e) => {
+            if (ref.current && !ref.current.contains(e.target)) setIsOpen(false);
+        };
+        document.addEventListener("mousedown", handleClick);
+        return () => document.removeEventListener("mousedown", handleClick);
+    }, []);
+
+    const handleSelect = (item) => {
+        setSelect(item);
+    };
+
   return (
     <div ref={ref}>
         <button onClick={() => setIsOpen(prev => !prev)}>
