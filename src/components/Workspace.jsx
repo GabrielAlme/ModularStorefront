d732ac4cbdf2c377ed9c7e0ef6a374469503ae6e
Gabriel Alme <gabealmeemail@gmail.com>
2026-09-25 08:44:13 -0500

    added function for reading the local storage for the previous layout if there was one and parsing the data into values that are readable by workspace

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -6,6 +6,11 @@ const Workspace = () => {
   const [panels, setPanels] = useState(workspace.panels);
   const nextId = useRef(workspace.nextId)
 
+  function loadWorkspace() {
+    const saved = localStorage.getItem("workspace");
+    return saved ? JSON.parse(saved) : { panels: [], nextID: 1 }; //checks local storage for the stored layout of panels, if there is no layout stored it upates it to the values on the right
+  }
+
   return (
     <div>
       
