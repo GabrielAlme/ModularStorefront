3803a92c5842688f807388a9c1116a531eb3c1f9
Gabriel Alme <gabealmeemail@gmail.com>
2026-09-25 11:03:54 -0500

    commented mosaic workspace

--- src/components/MosaicWorkspace.jsx
+++ src/components/MosaicWorkspace.jsx
@@ -9,12 +9,12 @@ const MosaicWorkspace = ({ panels }) => {
     <div style={{ height: "100vh" }}>
       <Mosaic
         className="mosaic-theme"
-        key={panels.map(p => p.id).join(",")}
-        initialValue={createBalancedTreeFromLeaves(panels.map(p => p.id))}
+        key={panels.map(p => p.id).join(",")} //takes the panels and maps them to a string that is used by initial value
+        initialValue={createBalancedTreeFromLeaves(panels.map(p => p.id))}//takes the string of values and builds the tree for the layout of the page
         zeroStateView={<h1>Add a panel from the dropdown menu</h1>}
-        renderTile={(id, path) => {
-            const panel = panels.find(p => p.id === id);
-            const { title, component: Component } = PANELS[panel.type];
+        renderTile={(id, path) => {//moves through the tree and creates a mosaic window for each of the leaves it hits
+            const panel = panels.find(p => p.id === id);//creates an object for each panel based on the id not the type
+            const { title, component: Component } = PANELS[panel.type];//uses the type of the panel to get the component for building the panel
             return (
             <MosaicWindow path={path} title={title}>
                 <Component/>
