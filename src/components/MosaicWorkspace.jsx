4b2f233d60cfd0a94466a4072a06da5d56c86e75
Gabriel Alme <gabealmeemail@gmail.com>
2026-09-25 16:05:50 -0500

    panels being added no longer cause the layout to be reset

--- src/components/MosaicWorkspace.jsx
+++ src/components/MosaicWorkspace.jsx
@@ -2,15 +2,15 @@ import { Mosaic, MosaicWindow, createBalancedTreeFromLeaves } from 'react-mosaic
 import 'react-mosaic-component/react-mosaic-component.css';
 import { PANELS } from '../panels/registry';
 
-const MosaicWorkspace = ({ panels }) => {
+const MosaicWorkspace = ({ panels, layout, onChange }) => {
 
 
   return (
     <div style={{ height: "100vh" }}>
       <Mosaic
         className="mosaic-theme"
-        key={panels.map(p => p.id).join(",")} //takes the panels and maps them to a string that is used by initial value
-        initialValue={createBalancedTreeFromLeaves(panels.map(p => p.id))}//takes the string of values and builds the tree for the layout of the page
+        value={layout}
+          onChange={onChange}
         zeroStateView={<h1>Add a panel from the dropdown menu</h1>}
         renderTile={(id, path) => {//moves through the tree and creates a mosaic window for each of the leaves it hits
             const panel = panels.find(p => p.id === id);//creates an object for each panel based on the id not the type
