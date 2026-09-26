1d640d11a77d8abbf7e16b02ba982fd81f5487cd
Gabriel Alme <gabealmeemail@gmail.com>
2026-09-26 10:46:08 -0500

    Added functionality to remove panels and scrub them from the local storage

--- src/components/MosaicWorkspace.jsx
+++ src/components/MosaicWorkspace.jsx
@@ -13,7 +13,7 @@ const MosaicWorkspace = ({ panels, layout, onChange }) => {
         onChange={onChange}
         zeroStateView={<h1>Add a panel from the dropdown menu</h1>}
         renderTile={(id, path) => {//moves through the tree and creates a mosaic window for each of the leaves it hits
-            const panel = panels.find(p => p.id === id);//creates an object for each panel based on the id not the type
+            const panel = panels[id];//creates an object for each panel based on the id not the type
             const { title, component: Component } = PANELS[panels[id].type];//uses the type of the panel to get the component for building the panel
             return (
             <MosaicWindow path={path} title={title}>
